699d716890f62a1acae8b5d9bc42c62115c73084
Robert Willemelis <robert.dev@gmx.de>
2020-04-13 17:52:20 +0200

    chore: add fix for path references of scully-routes.json

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "running postinstall script"
+echo "=== running postinstall script to fix path references to scully-routes.json ==="
+
+find node_modules/@scullyio/ -type f -exec sed -i "s/\/assets\/scully-routes\.json/\.\/assets\/scully-routes\.json/g" {} ';'
